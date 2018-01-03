7cff7f600aa91d8a10dc38c15ffa24a7f7e07e96
Barnack <sephirothbahamut@live.it>
2018-01-03 20:03:28 +0100

    idk what the hell happened to this repository during holydais

---
+++ pages/game_maker/controller.js
@@ -0,0 +1,32 @@
+﻿var fs = require('fs');
+var ejs = require("ejs");
+
+exports.send_page = function send_page(req, res)
+	{
+	
+	fs.readFile(__dirname + "/../global/header.ejs", function(err, header) 
+		{
+		fs.readFile(__dirname + "/../global/navbar.ejs", function(err, navbar) 
+			{
+			fs.readFile(__dirname + "/page.ejs", function(err, page) 
+				{
+				var args = 
+					{
+					color_scheme: "dark"
+					};
+				
+				var parts =
+					{
+					navbar: ejs.render(navbar.toString(), args),
+					header: ejs.render(header.toString(), args)
+					};
+				
+				res.writeHead(200, {'Content-Type': 'text/html'});
+				res.write(ejs.render(page.toString(), {parts: parts, args: args));
+				res.end();
+				
+				});
+			});
+		});
+	
+	}
---
+++ pages/sw/controller.js
@@ -0,0 +1,23 @@
+﻿var fs = require('fs');
+var ejs = require("ejs");
+
+exports.send_page = function send_page(req, res)
+	{
+	
+	fs.readFile(__dirname + "/../global/navbar.ejs", function(err, navbar) 
+		{
+		fs.readFile(__dirname + "/page.ejs", function(err, page) 
+			{
+			var args = 
+				{
+				navbar: ejs.render(navbar.toString(), {})
+				};
+			
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.write(ejs.render(page.toString(), args));
+			res.end();
+			
+			});
+		});
+	
+	}
